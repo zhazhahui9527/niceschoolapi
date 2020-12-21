c1a9d2c5e7d66c049778ee7094e9c87b6efe8cff
linly <2219376987@qq.com>
2020-12-21 17:47:01 +0800

    2020/12/21 17:46

--- src/main/java/com/nsapi/niceschoolapi/entity/Group.java
+++
@@ -1,9 +0,0 @@
-//package com.nsapi.niceschoolapi.entity;
-//
-//import com.baomidou.mybatisplus.annotation.TableName;
-//import com.nsapi.niceschoolapi.common.base.TreeEntity;
-//
-//@TableName("sys_group")
-//public class Group extends TreeEntity<Group> {
-//    private static final long serialVersionUID = 1L;
-//}
